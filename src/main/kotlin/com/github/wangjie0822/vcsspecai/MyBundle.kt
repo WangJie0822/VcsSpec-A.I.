49029efaebc5789748f02308737c3302021b2c49
GitHub Action <action@github.com>
2024-07-29 06:56:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangjie0822/vcsspecai/MyBundle.kt
+++ src/main/kotlin/com/github/wangjie0822/vcsspecai/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wangjie0822.vcsspecai
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wangjie0822/vcsspecai/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/wangjie0822/vcsspecai/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wangjie0822.vcsspecai.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
